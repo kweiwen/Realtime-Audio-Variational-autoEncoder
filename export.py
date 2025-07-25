945e0de02c4b6014774b9984a447a6ef4af616b2
kweiwen <kweiwen.tseng@gmail.com>
2025-07-26 00:29:13 +0800

    Adds initial RAVE model configuration
    Introduces the initial configuration files for the RAVE model,
    including the core gin configuration and hparams.yaml files for
    different versions. Also, adds a script for preprocessing the electone dataset using the rave tool.

---
+++ export.py
@@ -0,0 +1,2 @@
+# model.RAVE.n_channels = 2
+# rave export --run .  --channels 2 --name name_of_run --output ~/Desktop --streaming True
---
+++ preprocessed.py
@@ -0,0 +1,14 @@
+import subprocess
+
+dataset = "electone" #@param {type:"string"}
+channels = 2 #@param {type:"integer"}
+preprocessed_dataset = "electone/preprocessed" #@param {type:"string"}
+
+command = [
+    "rave", "preprocess",
+    "--input_path", dataset,
+    "--output_path", preprocessed_dataset,
+    "--channels", str(channels)
+]
+
+subprocess.run(command, check=True)
