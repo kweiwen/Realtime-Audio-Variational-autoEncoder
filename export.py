78fe862f1a8890cd88f55ca48c21ad5419accb3e
kweiwen <kweiwen.tseng@gmail.com>
2025-07-27 17:14:00 +0800

    Adds youtube-dl commands for playlist extraction
    Adds commands to extract audio from specified YouTube playlists.
    This allows for the automated downloading of audio content from
    multiple sources for further processing.

--- export.py
+++ export.py
@@ -1,2 +1,11 @@
 # model.RAVE.n_channels = 2
 # rave export --run .  --channels 2 --name name_of_run --output ~/Desktop --streaming True
+# yt-dlp -x --audio-format wav --audio-quality 10 -P .  "https://www.youtube.com/playlist?list=PL8hj_WcS6boTtrNxjNY_tR-gmgDDFYrjB"
+
+
+# yt-dlp -x --audio-format wav --audio-quality 10 -P .  "https://www.youtube.com/playlist?list=OLAK5uy_k_l-e8vDMUjgKaHVMVQifgmsrQgYddIWg"
+# yt-dlp -x --audio-format wav --audio-quality 10 -P .  "https://www.youtube.com/playlist?list=OLAK5uy_mKyfSWy-Kpm1uBbQLg2_0fvtAYbOfE9Vs"
+# yt-dlp -x --audio-format wav --audio-quality 10 -P .  "https://www.youtube.com/playlist?list=OLAK5uy_m8Uk4XV2Nfkv5BtRxZC8ZgCv9CdEg0nTw"
+# yt-dlp -x --audio-format wav --audio-quality 10 -P .  "https://www.youtube.com/playlist?list=OLAK5uy_lCU4KIWcOAxpfyNXNpP7YNJa5F72P2rnY"
+
+
